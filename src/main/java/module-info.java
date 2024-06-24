d3743cf8df98797a1db42a559293a849e35cd608
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-06-24 16:04:58 +0300

    .

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,7 +4,7 @@ module com.tugalsan.api.sql.min {
     requires com.tugalsan.api.tuple;
     requires com.tugalsan.api.validator;
     requires com.tugalsan.api.callable;
-    requires com.tugalsan.api.runnable;
+    
     requires com.tugalsan.api.log;
     requires com.tugalsan.api.sql.sanitize;
     requires com.tugalsan.api.sql.select;
