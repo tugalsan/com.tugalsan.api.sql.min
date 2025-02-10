8d3e326dd1a9d2da30d9891144a47e3a29ae0eb0
tugalsan karabacak <tugalsan@users.noreply.github.com>
2025-02-10 11:51:25 +0300

    .

--- src/main/java/com/tugalsan/api/sql/min/server/TS_SQLMin.java
+++ src/main/java/com/tugalsan/api/sql/min/server/TS_SQLMin.java
@@ -1,7 +1,7 @@
 package com.tugalsan.api.sql.min.server;
 
 
-import com.tugalsan.api.function.client.TGS_Func_In1;
+import com.tugalsan.api.function.client.maythrow.uncheckedexceptions.TGS_FuncMTUCE_In1;
 import com.tugalsan.api.sql.conn.server.*;
 import com.tugalsan.api.sql.where.server.*;
 
@@ -12,23 +12,23 @@ public class TS_SQLMin {
     }
     final private TS_SQLMinExecutor executor;
 
-     public TS_SQLMinValue whereGroupAnd(TGS_Func_In1<TS_SQLWhereGroups> groups) {
+     public TS_SQLMinValue whereGroupAnd(TGS_FuncMTUCE_In1<TS_SQLWhereGroups> groups) {
         executor.where = TS_SQLWhereUtils.where();
         executor.where.groupsAnd(groups);
         return new TS_SQLMinValue(executor);
     }
 
-    public TS_SQLMinValue whereGroupOr(TGS_Func_In1<TS_SQLWhereGroups> groups) {
+    public TS_SQLMinValue whereGroupOr(TGS_FuncMTUCE_In1<TS_SQLWhereGroups> groups) {
         executor.where = TS_SQLWhereUtils.where();
         executor.where.groupsOr(groups);
         return new TS_SQLMinValue(executor);
     }
 
-    public TS_SQLMinValue whereConditionAnd(TGS_Func_In1<TS_SQLWhereConditions> conditions) {
+    public TS_SQLMinValue whereConditionAnd(TGS_FuncMTUCE_In1<TS_SQLWhereConditions> conditions) {
         return whereGroupAnd(where -> where.conditionsAnd(conditions));
     }
 
-    public TS_SQLMinValue whereConditionOr(TGS_Func_In1<TS_SQLWhereConditions> conditions) {
+    public TS_SQLMinValue whereConditionOr(TGS_FuncMTUCE_In1<TS_SQLWhereConditions> conditions) {
         return whereGroupOr(where -> where.conditionsOr(conditions));
     }
 
